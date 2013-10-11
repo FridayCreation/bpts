c60c25b82ad223c8992df9d4961cd759e9c82c8e
woying <loveson821@gmail.com>
2013-10-11 13:42:48 +0800

    slider #3

--- Gruntfile.js
+++ Gruntfile.js
@@ -330,7 +330,7 @@ module.exports = function (grunt) {
                 'compass',
                 'copy:styles',
                 'imagemin',
-                'svgmin',
+//                'svgmin',
                 'htmlmin'
             ]
         },
